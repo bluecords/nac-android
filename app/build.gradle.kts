7fbb0244d33243614c62ec28674b0ee82d5795fa
bluecords <133072610+bluecords@users.noreply.github.com>
2026-06-22 07:10:43 -0400

    Update version to 1.6.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,8 +63,8 @@ android {
         applicationId = "nac.chat"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = Integer.parseInt("001_006_009".replace("_", ""), 10)
-        versionName = "1.6.9"
+        versionCode = Integer.parseInt("001_006_010".replace("_", ""), 10)
+        versionName = "1.6.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
